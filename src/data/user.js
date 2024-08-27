f84c364a6afcae423f0d33ee29fa5acf48c6d436
Barış <98917123+ElbaNrossSS@users.noreply.github.com>
2024-08-27 11:31:16 +0300

    Update user.js

--- src/data/user.js
+++ src/data/user.js
@@ -1,43 +1,72 @@
-import { useState, useEffect } from "react"
-import User from "./variables"
-import useSWR from "swr"
+import { useState, useEffect } from "react";
+import User from "./variables";
+import useSWR from "swr";
 
 export default function UserInformation() {
+    const [user, setUser] = useState(null);
+    const [github, setGithub] = useState([]);
+    const [discordStatus, setDiscordStatus] = useState('Unknown'); // Discord durumunu ekleyin
+    const [statusColor, setStatusColor] = useState('bg-gray-500'); // Durum rengini ekleyin
 
-    const [user, setUser] = useState(null)
-    const [github, setGithub] = useState([])
     let websocket;
-    let variables = {user, setUser, github}
-    let fetchData = (url) => fetch(url).then(r => r.json())
-    let {data, error} = useSWR('https://api.github.com/users/' + User.github + '/repos',fetchData,{refreshInterval:5000})
+    let variables = { user, setUser, github, discordStatus, statusColor };
+
+    let fetchData = (url) => fetch(url).then(r => r.json());
+    let { data, error } = useSWR('https://api.github.com/users/' + User.github + '/repos', fetchData, { refreshInterval: 5000 });
 
     useEffect(() => {
-        setGithub(data)
-    }, [data])
+        setGithub(data);
+    }, [data]);
 
     useEffect(() => {
+        websocket = new WebSocket("wss://api.lanyard.rest/socket");
 
-        websocket = new WebSocket("wss://api.lanyard.rest/socket")
-
-        websocket.onmessage = data => {
-            var message = JSON.parse(data.data)
-            if (message.op) { 
-                if (message.op == 1) {
+        websocket.onmessage = (data) => {
+            var message = JSON.parse(data.data);
+            if (message.op) {
+                if (message.op === 1) {
                     setInterval(() => {
-                        websocket.send(JSON.stringify({ op: 3 }))
-                    }, message.d.heartbeat_interval-1000)
-                    websocket.send(JSON.stringify({ op: 2,  d: {subscribe_to_ids: [User.userId]} }))
+                        websocket.send(JSON.stringify({ op: 3 }));
+                    }, message.d.heartbeat_interval - 1000);
+                    websocket.send(JSON.stringify({ op: 2, d: { subscribe_to_ids: [User.userId] } }));
                 }
             }
-            if (message.t && (message.t == "INIT_STATE" || message.t == "PRESENCE_UPDATE")) {
-                setUser((message.t == "PRESENCE_UPDATE") ? message.d : message.d[User.userId])
+            if (message.t && (message.t === "INIT_STATE" || message.t === "PRESENCE_UPDATE")) {
+                const userData = message.t === "PRESENCE_UPDATE" ? message.d : message.d[User.userId];
+                setUser(userData);
+
+                // Discord durumunu ayarla
+                if (userData) {
+                    switch (userData.discord_status) {
+                        case 'online':
+                            setDiscordStatus('Online');
+                            setStatusColor('bg-green-500');
+                            break;
+                        case 'idle':
+                            setDiscordStatus('Idle');
+                            setStatusColor('bg-yellow-500');
+                            break;
+                        case 'dnd':
+                            setDiscordStatus('Do not disturb');
+                            setStatusColor('bg-red-500');
+                            break;
+                        case 'invisible':
+                            setDiscordStatus('Invisible');
+                            setStatusColor('bg-gray-500');
+                            break;
+                        default:
+                            setDiscordStatus('Unknown');
+                            setStatusColor('bg-gray-500');
+                            break;
+                    }
+                }
             }
-        }
+        };
 
+        return () => {
+            websocket.close();
+        };
+    }, []);
 
-
-    }, [])
-
-    return variables
-
+    return variables;
 }
