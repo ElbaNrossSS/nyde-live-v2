796210b580c3236838bbf9a551f8816fcf9a45b3
Barış <98917123+ElbaNrossSS@users.noreply.github.com>
2024-08-28 15:33:20 +0300

    Update projects.js

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,13 +2,7 @@ export default {
 
     active: [
 
-        {
-            name: "Code Log",
-            description: "Find the best discord bot for your server in Code Log and upgrade your discord experience.",
-            image: "https://cdn.discordapp.com/attachments/1063792679214907523/1066125503758610504/Ekran_Alnts.PNG",
-            role: "Founder",
-            link: "https://codelog.glitch.me"
-        },
+
         {
             name: "BotList | Uptime Hizmeti",
             description: "Botlist host Invitation to come",
