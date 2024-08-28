8286b382a1cb16982ddd0bde23ccb583978fcdd7
Barış <98917123+ElbaNrossSS@users.noreply.github.com>
2024-08-28 15:21:56 +0300

    Update projects.js

--- src/data/projects.js
+++ src/data/projects.js
@@ -10,9 +10,9 @@ export default {
             link: "https://codelog.glitch.me"
         },
         {
-            name: "BotList 3.8K | Uptime Hizmeti  ᴍᴏᴏɴ",
+            name: "BotList | Uptime Hizmeti",
             description: "Botlist host Invitation to come",
-            image: "https://cdn.discordapp.com/attachments/1063792679214907523/1066266892840468500/Ekran_Alnts.PNG",
+            image: "https://cdn.discordapp.com/attachments/944629878714691656/1278328640077299742/site_icin_banner_1.png?ex=66d067d1&is=66cf1651&hm=9340eb2ee67531534d0a31f1a481ce93c19daca8196a2067c7f27f748adc6bb2&",
             role: "Founder",
             link: "https://discord.gg/GBsNQ7bTrm"
         }
