99b3368e782045757f5219a1881341b2cbf2a698
Barış <98917123+ElbaNrossSS@users.noreply.github.com>
2024-08-28 15:13:18 +0300

    Update variables.js

--- src/data/variables.js
+++ src/data/variables.js
@@ -2,11 +2,11 @@ export default {
 
     userId: "1045401575348256858",
     github: "ElbaNrossSS",
-    description: "My name is Barış, I am 16 years old Turkish student. I interested in Software and Design. I can develop front-end and back-end websites for now.",
+    description: "My name is Barış, I am 18 years old Turkish student. I interested in Software and Design. I can develop front-end and back-end websites for now.",
     about: {
         description: "Hello. My name is <span class='text-primary'>Barış</span>. I am <span class='text-primary'>16 years</span> old. I am a <span class='text-primary'>3rd year student</span> and I interested in <span class='text-primary'>Software</span>. I've been coding <span class='text-primary'>since 2019</span> and I want to improve myself in this field.",
         liveIn: "Nevşehir, Gülşehir",
-        birthday: "16 August, 2006",
+        birthday: "5 August, 2006",
         gender: "Man"
     }
 
